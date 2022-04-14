eb444ca8987b137c7d30c192f51b5f2e6202ccbb
JustAnotherUselessAccount <unalimoff@yandex.ru>
2022-04-14 19:22:15 +0300

    hhh

--- app/square_root.h
+++ app/square_root.h
@@ -6,8 +6,8 @@ int get_roots(double a, double b, double c, double* x1, double* x2) {
         return 1;       // Ошибка - ошибочное выражение
     }
     if (a == 0) {
-        *x1 = -c;
-        *x2 = -c;
+        *x1 = -c/b;
+        *x2 = -c/b;
         return 0;       // Возврат - один корень
     }
     double D = b*b - 4 * a * c;
